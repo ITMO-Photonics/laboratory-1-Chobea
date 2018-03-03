dcbb2253d983b53ffdac35b065a8e57a57b940a0
user1 <vk17@bk.com>
2018-03-03 11:04:53 +0300

    Task1 done

--- task1.py
+++ task1.py
@@ -1,1 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+print('Введите значение длины цилиндра L')
+L = int(input())
+print('Введите значение радиуса цилиндра R')
+R = int(input())
+V = 3.14 * R * R * L
+print('Объём цилиндра равен', V)
+
+
 
