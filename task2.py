fba502f884e893a5be92fa34ecf40c6b2f8695c0
Chobea <vk17@bk.ru>
2018-04-13 13:56:55 +0300

    done

--- task2.py
+++ task2.py
@@ -1,5 +1,5 @@
 import numpy as np
-#import timeit
+import timeit
 from scipy import optimize
 
 x=np.linspace(0.1,2.4,100)
@@ -9,19 +9,30 @@ def fprime(x):
 	return (-np.sin(x)*(1+x*x)-2*x*np.cos(x))/((1+x*x)**2)
 
 print ('Bisection method')
+start = timeit.default_timer()
 root1 = optimize.bisect(f,0.1,2.4)
+stop = timeit.default_timer()-start
 print (root1)
-#%timeit optimize.newton(f,0.1,2.4)
+print(stop)
 
 print('Newton-Raphson method')
+start = timeit.default_timer()
 root2 = optimize.newton(f,1.5)
+stop = timeit.default_timer()-start
 print (root2)
+print(stop)
 
 print('Secant method')
+start = timeit.default_timer()
 root3 = optimize.newton(f,1.5,fprime)
+stop = timeit.default_timer()-start
 print (root3)
+print(stop)
 
 print('Brents method')
+start = timeit.default_timer()
 root4 = optimize.brentq(f,0.1,2.4)
+stop = timeit.default_timer()-start
 print (root4)
+print(stop)
 
--- task4.py
+++ task4.py
@@ -1,1 +1,31 @@
+import numpy as np
+import scipy
+import timeit
+N = 50
+A = np.random.random((N,N))
+k = np.arange(N)
+def b(k,t):
+	return (k/(1+k*t))
 
+start = timeit.default_timer()
+for t in np.linspace(0,10,100):
+	x1 = scipy.linalg.solve(A,b(k,t))
+	#print (x1)
+stop = timeit.default_timer()-start
+print(stop)
+
+start = timeit.default_timer()
+LU = scipy.linalg.lu_factor(A)
+for t in np.linspace(0,10,100):
+	x2 = scipy.linalg.lu_solve(LU,b(k,t))
+	#print (x2)
+stop = timeit.default_timer()-start
+print(stop)
+
+start = timeit.default_timer()
+Ainv = scipy.linalg.inv(A)
+for t in np.linspace(0,10,100):
+	x3 = np.dot(A,b(k,t))
+	#print (x3)
+stop = timeit.default_timer()-start
+print(stop)
