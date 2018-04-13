03ae286e14a4caa5c64804ede78a382efef8db94
Chobea <vk17@bk.ru>
2018-04-13 12:56:22 +0300

    done

--- task2.py
+++ task2.py
@@ -1,15 +1,27 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import timeit
 from scipy import optimize
+
 x=np.linspace(0.1,2.4,100)
 def f(x):
 	return (np.cos(x)/(1+x*x))
+def fprime(x):
+	return (-np.sin(x)*(1+x*x)-2*x*np.cos(x))/((1+x*x)**2)
+
+print ('Bisection method')
 root1 = optimize.bisect(f,0.1,2.4)
 print (root1)
+#%timeit optimize.newton(f,0.1,2.4)
+
+print('Newton-Raphson method')
 root2 = optimize.newton(f,1.5)
 print (root2)
-#root3 = optimize.newton(f,1.5,fprime=lambda x: -np.sin(x)*(1+x*x)-2*x*np.cos(x)/(1+x*x)**2)) - error
-#print (root3)
+
+print('Secant method')
+root3 = optimize.newton(f,1.5,fprime)
+print (root3)
+
+print('Brents method')
 root4 = optimize.brentq(f,0.1,2.4)
 print (root4)
-#%time optimize.newton(f,0)
+
--- task3.py
+++ task3.py
@@ -1,1 +1,11 @@
-
+import numpy as np
+n = 100
+A = np.zeros((n,n))
+i,j = np.indices(A.shape)
+for k in range (n):
+	A[i==j+k]=1
+B = np.arange(n)
+X = np.linalg.solve(A,B)
+print (A)
+print (B)
+print (X)
